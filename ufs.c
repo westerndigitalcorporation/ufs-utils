d277cca1434f442c65041eddf2e136f55cb6d74f
Yuli Izrailov <yuli.izrailov@sandisk.com>
2019-11-24 10:40:32 +0200

    Update tool version to 1.6
    Change-Id: I891b3a8cbbef89e5e26495cb7625690ba4949ac1

--- ufs.c
+++ ufs.c
@@ -21,7 +21,7 @@
 #include "ufs_rpmb.h"
 #include "ufs_hmr.h"
 
-#define UFS_BSG_UTIL_VERSION	"1.5"
+#define UFS_BSG_UTIL_VERSION	"1.6"
 typedef int (*command_function)(struct tool_options *opt);
 
 struct tool_command {
