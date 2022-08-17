9cfecbf902d8ba24a881a11826246880e80e7c87
Arthur Simchaev <Arthur.Simchaev@wdc.com>
2022-08-17 16:06:51 +0300

    Promote version to 3.11.3
    Change-Id: If88d3c733436a73b11ec03cd787be0e72a37a665

--- ufs.c
+++ ufs.c
@@ -21,7 +21,7 @@
 #include "ufs_rpmb.h"
 #include "ufs_hmr.h"
 
-#define UFS_BSG_UTIL_VERSION	"3.11.2"
+#define UFS_BSG_UTIL_VERSION	"3.11.3"
 
 typedef int (*command_function)(struct tool_options *opt);
 
